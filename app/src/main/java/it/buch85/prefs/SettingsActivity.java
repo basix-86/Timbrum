a914cf2c5d951533c5685f2a57f896532fd50b83
mmatessi <17149962+basix86@users.noreply.github.com>
2021-07-26 15:20:11 +0200

    working SettingsActivity activity

--- app/src/main/java/it/buch85/prefs/SettingsActivity.java
+++ app/src/main/java/it/buch85/prefs/SettingsActivity.java
@@ -1,16 +1,38 @@
 package it.buch85.prefs;
 
-import it.buch85.R;
-import it.buch85.R.xml;
 import android.os.Bundle;
 import android.preference.PreferenceActivity;
+import android.preference.PreferenceFragment;
+
+import java.util.List;
+
+import it.buch85.R.xml;
 
 
 public class SettingsActivity extends PreferenceActivity  {
-	@Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        addPreferencesFromResource(R.xml.preferences);
+
+    @Override
+    public void onBuildHeaders(List<Header> target) {
+        loadHeadersFromResource(xml.preference_headers, target);
+    }
+
+    @Override
+    protected boolean isValidFragment(String fragmentName) {
+        if (First.class.getName().equals(fragmentName)
+                ) {
+            return(true);
+        }
+
+        return(false);
+    }
+
+    public static class First extends PreferenceFragment {
+        @Override
+        public void onCreate(Bundle savedInstanceState) {
+            super.onCreate(savedInstanceState);
+
+            addPreferencesFromResource(xml.preferences);
+        }
     }
 
 }
