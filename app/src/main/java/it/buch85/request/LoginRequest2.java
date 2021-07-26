dcbe67c2eae34f0c3998c607d90b5d6a50b8e476
mmatessi <17149962+basix86@users.noreply.github.com>
2021-07-26 16:52:26 +0200

    status code

--- app/src/main/java/it/buch85/request/LoginRequest2.java
+++ app/src/main/java/it/buch85/request/LoginRequest2.java
@@ -5,6 +5,7 @@ import android.content.Context;
 import android.util.Log;
 
 import com.android.volley.AuthFailureError;
+import com.android.volley.NetworkResponse;
 import com.android.volley.Request;
 import com.android.volley.RequestQueue;
 import com.android.volley.Response;
@@ -49,7 +50,10 @@ public class LoginRequest2 {
     }
 
 
+    private int mStatusCode;
+
     public void submit2() throws IOException {
+
 
 
         RequestQueue queue = Volley.newRequestQueue(context);
@@ -59,7 +63,7 @@ public class LoginRequest2 {
                 new Response.Listener<String>() {
                     @Override
                     public void onResponse(String response) {
-                        Log.e("HttpClient", "success! response: " + response.toString());
+                        Log.e("HttpClient", "status code: " + mStatusCode +" success! response: " + response.toString());
                     }
                 },
                 new Response.ErrorListener() {
@@ -68,12 +72,27 @@ public class LoginRequest2 {
                         Log.e("HttpClient", "error: " + error.toString());
                     }
                 }) {
+
+            ;
+
+            @Override
+            protected void deliverResponse(String response) {
+                super.deliverResponse(response);
+            }
+
             @Override
             protected Map<String, String> getParams() {
                 Map<String, String> params = new HashMap<String, String>();
                 params.put(USERNAME_FIELD, username);
                 params.put(PASSWORD_FIELD, password);
+                params.put(ACTION_FIELD, ACTION_FIELD_VALUE);
                 return params;
+            }
+
+            @Override
+            protected Response<String> parseNetworkResponse(NetworkResponse response) {
+                mStatusCode = response.statusCode;
+                return super.parseNetworkResponse(response);
             }
 
             @Override
